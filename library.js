177145d8f779e8062595ce4125946e825aaf6526
Tresor11 <tresor.moise2001@gmail.com>
2020-03-12 17:05:16 +0200

    fixed the no-tab erros

--- library.js
+++ library.js
@@ -12,24 +12,24 @@ function render() {
   let tableRows = '';
 
   myLibrary.forEach(book => {
-    tableRows += `<tr>
-		<th scope='row' class='text-center'><span class='badge badge-primary'>${
-      book.id
-    }</span></th>
-		<th class='text-center'> ${book.title}</th>
-		<th class='text-center'>${book.author}</th>
-		<th class='text-center'>${book.pages}</th>
-		<th class='text-center'><button type='button' class='btn btn-outline-primary' onclick='changeStatus(${
+  tableRows += `<tr>
+<th scope='row' class='text-center'><span class='badge badge-primary'>${
+  book.id
+}</span></th>
+<th class='text-center'> ${book.title}</th>
+<th class='text-center'>${book.author}</th>
+<th class='text-center'>${book.pages}</th>
+<th class='text-center'><button type='button' class='btn btn-outline-primary' onclick='changeStatus(${
   book.id
 })'>
-		${book.status ? 'Read' : 'Not read'}
-		</button>
-		</th>
-		<th class='text-center'><button type='button' class='btn btn-outline-danger' onclick='deleteBook(${
-      book.id
+  ${book.status ? 'Read' : 'Not read'}
+</button>
+</th>
+<th class='text-center'><button type='button' class='btn btn-outline-danger' onclick='deleteBook(${
+  book.id
 })'>
-		Delete
-		</button></th>
+Delete
+</button></th>
 </tr>`;
   });
   document.getElementById('table-body').innerHTML = tableRows;
