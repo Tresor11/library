836449cd842eb16e6cab8e8ec3f7883f39ad0852
Tresor11 <tresor.moise2001@gmail.com>
2020-03-12 12:54:52 +0200

    fixed the formatting issues

--- library.js
+++ library.js
@@ -1,75 +1,77 @@
 const myLibrary = [];
 
 function Book(title, author, pages, status) {
-    this.title = title;
-    this.id = myLibrary.length
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
+  this.title = title;
+  this.id = myLibrary.length;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
 }
 
 function render() {
-    let tableRows = ''
+  let tableRows = "";
 
-    myLibrary.forEach(book => {
-        tableRows += `<tr>
+  myLibrary.forEach(book => {
+    tableRows += `<tr>
 		<th scope="row">${book.id}</th>
 		<td class="text-center">${book.title}</td>
 		<td class="text-center">${book.author}</td>
 		<td class="text-center">${book.pages}</td>
-		<td class="text-center"><button type="button" onclick="changeStatus(${book.id})">
-		${book.status? 'Read':'Not read'}
+		<td class="text-center"><button type="button" onclick="changeStatus(${
+      book.id
+    })">
+		${book.status ? "Read" : "Not read"}
 		</button>
 		</td>
 		<td class="text-center"><button type="button" onclick="deleteBook(${book.id})">
 		Delete
 		</button></td>
-</tr>`
-    })
-    document.getElementById("table-body").innerHTML = tableRows;
+</tr>`;
+  });
+  document.getElementById("table-body").innerHTML = tableRows;
 }
 
 function addBookToLibrary() {
-    const title = document.getElementById("title").value;
-    const author = document.getElementById("author").value;
-    const pages = document.getElementById("pages").value;
-    const status = document.getElementById("status").checked;
-    const newBook = new Book(title, author, pages, status);
-    if (title == '') {
-        window.alert("please provide a title")
-        title.focus();
-        return false;
-    }
-    myLibrary.push(newBook);
-    displayN();
-    render();
-    return 0;
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const status = document.getElementById("status").checked;
+  const newBook = new Book(title, author, pages, status);
+  if (title == "") {
+    window.alert("please provide a title");
+    title.focus();
+    return false;
+  }
+  myLibrary.push(newBook);
+  displayN();
+  render();
+  return 0;
 }
 
 function changeStatus(id) {
-    myLibrary.forEach(book => {
-        if (book.id == id) {
-            book.status = !book.status
-        }
-    })
-    render();
+  myLibrary.forEach(book => {
+    if (book.id == id) {
+      book.status = !book.status;
+    }
+  });
+  render();
 }
 
 function deleteBook(id) {
-    myLibrary.forEach(book => {
-        if (book.id == id) {
-            myLibrary.splice(myLibrary.indexOf(book), 1)
-        }
-    })
-    render()
+  myLibrary.forEach(book => {
+    if (book.id == id) {
+      myLibrary.splice(myLibrary.indexOf(book), 1);
+    }
+  });
+  render();
 }
 
 const modal = document.getElementById("modal");
 
 function displayB() {
-    modal.style.display = "block";
+  modal.style.display = "block";
 }
 
 function displayN() {
-    modal.style.display = "none";
+  modal.style.display = "none";
 }
