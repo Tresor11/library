f6910a2db277a539a51f6870c91d6846659f56ee
Tresor11 <tresor.moise2001@gmail.com>
2020-03-13 11:26:25 +0200

    fixed the indentation errors

--- library.js
+++ library.js
@@ -14,20 +14,20 @@ function render() {
   myLibrary.forEach(book => {
     tableRows += `<tr>
 <th scope='row' class='text-center'><span class='badge badge-primary'>${
-      book.id
-    }</span></th>
+  book.id
+}</span></th>
 <th class='text-center'> ${book.title}</th>
 <th class='text-center'>${book.author}</th>
 <th class='text-center'>${book.pages}</th>
 <th class='text-center'><button type='button' class='btn btn-outline-primary' onclick='changeStatus(${
-      book.id
-    })'>
+  book.id
+})'>
   ${book.status ? 'Read' : 'Not read'}
 </button>
 </th>
 <th class='text-center'><button type='button' class='btn btn-outline-danger' onclick='deleteBook(${
-      book.id
-    })'>
+  book.id
+})'>
 Delete
 </button></th>
 </tr>`;
